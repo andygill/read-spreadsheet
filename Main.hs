78b8a853477f978aa7a8c784b0ca15dc638fec7b
Andy Gill <andygill@ku.edu>
2014-07-24 23:23:19 -0500

    Getting list of worksheets inside the spreadsheet

--- Main.hs
+++ Main.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 -- Show it is possible to use Google Sheets
 import Network.Curl
 import Data.Aeson
 import Data.ByteString (ByteString)
 import qualified Data.ByteString  as BS
+import qualified Data.ByteString.Lazy as BSL
 import Data.Aeson.Parser as Parser
 import Data.Attoparsec.ByteString.Char8
 import Data.Char
+import Data.Aeson.Encode.Pretty
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Vector as V
 
 main = withCurlDo $ do
   key <- readFile "my.key"
@@ -14,10 +19,16 @@ main = withCurlDo $ do
                 (unwords $ words $ key) ++
                 "/public/basic?alt=json")
 		         []
-  print code
-  print str
-  print (parseOnly Parser.value (myPack str))
-
+--  print code
+--  print str
+  let Right (Object obj :: Value) = parseOnly Parser.value (myPack str)
+  let Just (Object feed)   = HashMap.lookup "feed"  obj
+  let Just (Array entry)  = HashMap.lookup "entry" feed
+  sequence_ [ do let Just (Object c) = HashMap.lookup "content" e
+                 print c
+            | Object e <- V.toList entry
+            ]
+--  BSL.putStrLn (encodePretty entry)
 
 myPack :: String -> ByteString
 myPack = BS.pack . map (fromIntegral . ord)
